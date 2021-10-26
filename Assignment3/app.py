e278f6d6fbeac36df0019b38709c135685103fed
Jackson Dagger <jacksonddagger@gmail.com>
2021-10-25 22:45:40 -0700

    correct shared secret check

--- Assignment3/app.py
+++ Assignment3/app.py
@@ -156,8 +156,7 @@ class Assignment3VPN:
                     # Disabling the button to prevent repeated clicks
                     self.secureButton["state"] = "disabled"
                     # Processing the protocol message
-                    self.sharedSecret.get() # update shared secret
-                    self.prtcl.ProcessReceivedProtocolMessage(cipher_text, self.sharedSecret)
+                    self.prtcl.ProcessReceivedProtocolMessage(cipher_text, self.sharedSecret.get())
 
                 # Otherwise, decrypting and showing the messaage
                 else:
@@ -182,8 +181,7 @@ class Assignment3VPN:
         self.secureButton["state"] = "disabled"
 
         # TODO: THIS IS WHERE YOU SHOULD IMPLEMENT THE START OF YOUR MUTUAL AUTHENTICATION AND KEY ESTABLISHMENT PROTOCOL, MODIFY AS YOU SEEM FIT
-        self.sharedSecret.get()
-        init_message = self.prtcl.GetProtocolInitiationMessage(self.sharedSecret)
+        init_message = self.prtcl.GetProtocolInitiationMessage(self.sharedSecret.get())
         self._SendMessage(init_message)
 
 
