e758a588f6d94c23beb301c60bedd68774b1b7bb
Jackson Dagger <JacksonDDagger@gmail.com>
2021-10-29 18:50:41 -0700

    Add flag identifying protocol initiation messages

--- Assignment3/protocol.py
+++ Assignment3/protocol.py
@@ -34,8 +34,7 @@ class Protocol:
         self._AuthNonce     = None
         self._g             = 2
         self._p             = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
-        self._ClientInitVal = b'Client protocol initiation message'
-        self._ServerInitVal = b'Server protocol initiation message'
+        self._InitVal       = b'Protocol initiation message'
         self._MWait         = None
         pass
 
@@ -84,11 +83,11 @@ class Protocol:
         self._MWait = datetime.now()
 
         # Encrypting the message
-        return self.EncryptAndProtectProtocol(pickle.dumps(msg))
+        return self._InitVal + (self.EncryptAndProtectProtocol(pickle.dumps(msg)))
 
     def IsMessagePartOfProtocol(self, message):
         """Checking if a received message is part of your protocol (called from app.py)"""
-        if self._BootstrapKey == None or self._SessionKey == None:
+        if message[:len(self._InitVal)] == self._InitVal:
             return True
         return False
 
@@ -100,6 +99,7 @@ class Protocol:
         Exception: if authentication fails
         """
         # Decrypting the message
+        message = message[len(self._InitVal):]
         message = self.DecryptAndVerifyProtocol(message, secret)
         msg = pickle.loads(message)
         
@@ -134,7 +134,7 @@ class Protocol:
             self.SetSessionKey(msg["DiffieHellman"])
 
 
-            return self.EncryptAndProtectProtocol(pickle.dumps(resp))
+            return self._InitVal + self.EncryptAndProtectProtocol(pickle.dumps(resp))
         else:
             # We are processing a response
             self.SetSessionKey(msg["DiffieHellman"]) # For now put 1, still waiting for the decryption implmentation, so i can get the DH part key from the received message.
